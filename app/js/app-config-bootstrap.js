0bec2d751cbcf87d4764406e65c11b61feff8562
unknown <renatogbj@gmail.com>
2015-08-13 21:59:37 -0300

    Added angular-material

--- app/js/app-config-bootstrap.js
+++ app/js/app-config-bootstrap.js
@@ -1,4 +1,5 @@
 /**
  * Created by Renato Oobj on 12/08/2015.
  */
-angular.module('HelloWorldApp', ['ngMessages', 'ui.growl', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.selection']);
+angular.module('HelloWorldApp', ['ngMessages', 'ui.growl', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.selection',
+                                 'ngMaterial']);
